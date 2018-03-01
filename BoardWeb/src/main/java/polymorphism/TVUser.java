c37f5a5eb6f96595708e90283f470948b9cfc6ca
Jongsung-Kim <ziippy@naver.com>
2018-03-02 00:30:39 +0900

    CLASS 04 까지

--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -27,6 +27,9 @@ public class TVUser {
 		tv.volumeDown();
 		tv.powerOff();
 		
+		//TV tv2 = (TV)factory.getBean("tv");
+		//TV tv3 = (TV)factory.getBean("tv");
+		
 		// 3. Spring 컨테이너 종료
 		factory.close();
 		//*/
