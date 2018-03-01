77280bbc421a32d2db56814aa89affae3f26e0d6
Jongsung-Kim <ziippy@naver.com>
2018-03-02 07:53:55 +0900

    CLASS 06 작업중

--- BoardWeb/src/main/java/com/springbook/biz/impl/BoardDAO.java
+++ BoardWeb/src/main/java/com/springbook/biz/impl/BoardDAO.java
@@ -3,6 +3,8 @@ package com.springbook.biz.impl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
@@ -75,4 +77,62 @@ public class BoardDAO {
 		}
 	}
 	
+	// 글 상세 조회
+    public BoardVO getBoard(BoardVO vo){
+        System.out.println("===> JDBC로 getBoard 처리");
+        BoardVO board = null;
+        try {
+            conn = JDBCUtil.getConnection();        
+            stmt = conn.prepareStatement(BOARD_GET);
+            stmt.setInt(1, vo.getSeq());
+            rs = stmt.executeQuery();
+            if(rs.next()){
+                board = new BoardVO();
+                board.setSeq(rs.getInt("SEQ"));
+                board.setTitle(rs.getString("TITLE"));
+                board.setWriter(rs.getString("WRITER"));
+                board.setContent(rs.getString("CONTENT"));
+                board.setRegDate(rs.getDate("REGDATE"));
+                board.setCnt(rs.getInt("CNT"));
+            }    
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtil.close(rs, stmt, conn);
+        }
+        return board;
+    }
+    
+    // 글 목록 조회
+    public List<BoardVO> getBoardList(BoardVO vo){
+        System.out.println("===> JDBC로 getBoardList 기능 처리");
+        List<BoardVO> boardList = new ArrayList<BoardVO>();
+        try {
+            conn = JDBCUtil.getConnection();
+            /*
+            if(vo.getSearchCondition().equals("TITLE")) {
+                stmt = conn.prepareStatement(BOARD_LIST_T);
+            } else if(vo.getSearchCondition().equals("CONTENT")) {
+                stmt = conn.prepareStatement(BOARD_LIST_C);
+            }
+            stmt.setString(1, vo.getSearchKeyword());
+            */
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                BoardVO board = new BoardVO();
+                board.setSeq(rs.getInt("SEQ"));
+                board.setTitle(rs.getString("TITLE"));
+                board.setWriter(rs.getString("WRITER"));
+                board.setContent(rs.getString("CONTENT"));
+                board.setRegDate(rs.getDate("REGDATE"));
+                board.setCnt(rs.getInt("CNT"));
+                boardList.add(board);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtil.close(rs, stmt, conn);
+        }
+        return boardList;
+    } 
 }
