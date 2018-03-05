eb5fdb6e1bc73ba3f2a6ec93b82195e2ca8fb4e6
Jongsung-Kim <ziippy@naver.com>
2018-03-05 23:33:09 +0900

    .

--- MybatisProject/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ MybatisProject/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -18,37 +18,31 @@ public class BoardDAO {
 		mybatis = SqlSessionFactoryBean.getSqlSessionInstance();
 	}
 	
-	// CRUD 기능의 메소드 구현
-	// 글 등록
 	public void insertBoard(BoardVO vo) {
-		System.out.println("===> mybatis로 insertBoard 처리");
+		System.out.println("===> mybatis insertBoard 처리");
 		mybatis.insert("BoardDAO.insertBoard", vo);
 		mybatis.commit();
 	}
 
-	// 글 수정
 	public void updateBoard(BoardVO vo) {
-		System.out.println("===> mybatis로 updateBoard 처리");
+		System.out.println("===> mybatis updateBoard 처리");
 		mybatis.update("BoardDAO.updateBoard", vo);
 		mybatis.commit();
 	}
 
-	// 글 삭제
 	public void deleteBoard(BoardVO vo) {
-		System.out.println("===> mybatis로 deleteBoard 처리");
+		System.out.println("===> mybatis deleteBoard 처리");
 		mybatis.delete("BoardDAO.deleteBoard", vo);
 		mybatis.commit();
 	}
 
-	// 글 상세 조회
 	public BoardVO getBoard(BoardVO vo) {
-		System.out.println("===> mybatis로 getBoard 처리");
+		System.out.println("===> mybatis getBoard 처리");
 		return (BoardVO)mybatis.selectOne("BoardDAO.getBoard", vo);
 	}
 
-	// 글 목록 조회
 	public List<BoardVO> getBoardList(BoardVO vo) {
-		System.out.println("===> mybatis로 getBoardList 기능 처리");
+		System.out.println("===> mybatis getBoardList 기능 처리");
 		return mybatis.selectList("BoardDAO.getBoardList", vo);
 	}
 }
